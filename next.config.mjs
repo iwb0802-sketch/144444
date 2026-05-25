55b8a7b1f593e282f45939080221497df0c7dd98
iwb0802-sketch <iwb0802@gmail.com>
2026-05-25 13:30:37 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,42 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        has: [
+          {
+            type: "host",
+            value: "inus.weddingmusic.kr",
+          },
+        ],
+        destination: "/inus",
+        permanent: false,
+      },
+      {
+        source: "/",
+        has: [
+          {
+            type: "host",
+            value: "bns.weddingmusic.kr",
+          },
+        ],
+        destination: "/bnscustomer",
+        permanent: false,
+      },
+      {
+        source: "/",
+        has: [
+          {
+            type: "host",
+            value: "staff.weddingmusic.kr",
+          },
+        ],
+        destination: "/bns",
+        permanent: false,
+      },
+    ];
+  },
+};
+
 export default nextConfig;
