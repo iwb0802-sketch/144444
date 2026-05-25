f98a4430601c253c459090f3c23dfbb11a830bec
iwb0802-sketch <iwb0802@gmail.com>
2026-05-25 14:23:39 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,39 +2,10 @@
 const nextConfig = {
   async redirects() {
     return [
-      {
-        source: "/",
-        has: [
-          {
-            type: "host",
-            value: "inus.weddingmusic.kr",
-          },
-        ],
-        destination: "/inus",
-        permanent: false,
-      },
-      {
-        source: "/",
-        has: [
-          {
-            type: "host",
-            value: "bns.weddingmusic.kr",
-          },
-        ],
-        destination: "/bnscustomer",
-        permanent: false,
-      },
-      {
-        source: "/",
-        has: [
-          {
-            type: "host",
-            value: "staff.weddingmusic.kr",
-          },
-        ],
-        destination: "/bns",
-        permanent: false,
-      },
+      { source: "/", has: [{ type: "host", value: "inus.weddingmusic.kr" }], destination: "/inus", permanent: false },
+      { source: "/", has: [{ type: "host", value: "bns.weddingmusic.kr" }], destination: "/bnscustomer", permanent: false },
+      { source: "/", has: [{ type: "host", value: "staff.weddingmusic.kr" }], destination: "/bns", permanent: false },
+      { source: "/", has: [{ type: "host", value: "yedo.weddingmusic.kr" }], destination: "/yedo", permanent: false },
     ];
   },
 };
